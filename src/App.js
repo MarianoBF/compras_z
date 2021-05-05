481ea8a96581c9a6e7f7f9532c3fcbbc5db3f5a8
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-05-04 21:50:48 -0300

    update welcome message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Una aplicación creada con Create React App como base para un proyecto.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
