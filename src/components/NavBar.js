922878fc559c6a38afd08e58f7cfe41f88675c32
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-05-07 22:51:03 -0300

    adjust listing & format file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,18 +4,30 @@ import logo from "../logo192.png";
 
 function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="justify-content-end">
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      className="justify-content-end">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
       <Navbar.Collapse id="responsive-navbar-nav">
-      <Navbar.Brand href="#home"  className="justify-content-start" style={{ width: "5%" }}>
-      <img src={logo} roundedCircle alt="logo" className="logo" />&nbsp;
-      </Navbar.Brand>
-      
-        <Nav className="justify-content-end" style={{ width: "100%" }}>
-          <Nav.Link href="#productos">Productos</Nav.Link>
-          <Nav.Link eventKey={2} href="#marcas">
-            Marcas
+        <Navbar.Brand
+          href="#home"
+          className="justify-content-start"
+          style={{width: "5%"}}>
+          <img src={logo} roundedCircle alt="logo" className="logo" />
+          COMPRAS RR
+        </Navbar.Brand>
+
+        <Nav className="justify-content-end" style={{width: "100%"}}>
+          <Nav.Link href="#vehículos">Vehículos</Nav.Link>
+          <Nav.Link eventKey={2} href="#electrónica">
+            Electrónica
+          </Nav.Link>
+          <Nav.Link eventKey={2} href="#libros">
+            Libros
           </Nav.Link>
           <Nav.Link eventKey={2} href="#ofertas">
             Ofertas
