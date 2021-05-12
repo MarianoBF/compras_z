1d5047c30505fdca4f08e417a289f9aa649a55ed
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-05-12 11:06:48 -0300

    add styling to itemlistcontainer

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,15 +1,17 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+
 export default function ItemListContainer({name}) {
   return (
     <div>
       <h1 style={{textAlign: "center"}}>
-        Hola {name}, desde aquí podrás ver los productos agreegados a tu compra{" "}
+        Hola {name}, desde aquí podrás ver los productos agregados a tu compra{" "}
       </h1>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-      </ul>
+      <ListGroup>
+        <ListGroup.Item variant="info">Item 1</ListGroup.Item>
+        <ListGroup.Item variant="info">Item 2</ListGroup.Item>
+        <ListGroup.Item variant="info">Item 3</ListGroup.Item>
+        <ListGroup.Item variant="info">Item 4</ListGroup.Item>
+      </ListGroup>
     </div>
   );
 }
