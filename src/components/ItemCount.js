7f28f38ebe03f89579bb93bc1d602e635971ac47
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-05-14 21:50:02 -0300

    format & include max & min quantities

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,23 +1,34 @@
-import Button from 'react-bootstrap/Button';
-import {useState} from "react"
+import Button from "react-bootstrap/Button";
+import {useState} from "react";
 
-export default function ItemCount() {
+export default function ItemCount({stock, initialValue, handleAdd}) {
+  const [quantity, setQuantity] = useState(initialValue);
 
-const [quantity, setQuantity] = useState(0);
+  const handleMore = () => {
+    if (quantity < stock) {
+      setQuantity(quantity => quantity + 1);
+    }
+  };
 
-const handleMore = () => {
-    setQuantity(quantity=>quantity + 1)
-}
+  const handleLess = () => {
+    if (quantity > 1) {
+      setQuantity(quantity => quantity - 1);
+    }
+  };
 
-const handleLess = () => {
-    setQuantity(quantity=>quantity - 1)
-}
-
-    return(
-        <div>
-        <Button onClick={handleMore}>+</Button>
-        {quantity}
-        <Button onClick={handleLess}>-</Button>
-        </div>
-    )
+  return (
+    <>
+        <Button color="info" onClick={handleMore}>
+          {" "}
+          +{" "}
+        </Button>
+         {` ${quantity} `}
+        <Button color="info" onClick={handleLess}>
+          {" "}
+          -{" "}
+        </Button>
+        {" "}
+      <Button onClick={(e)=>handleAdd(e, quantity)}>Agregar al Carrito</Button>
+    </>
+  );
 }
