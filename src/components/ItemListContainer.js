590c4f24827f545ebca5cb20830fda237d59cc6a
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-05-14 21:50:37 -0300

    move itemcount to item list & mock handle addtocart

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,16 +1,66 @@
-import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroup from "react-bootstrap/ListGroup";
+import ItemCount from "./ItemCount";
+import {useState} from "react";
 
 export default function ItemListContainer({greeting}) {
+  const initialStocks = {
+    item1: 10,
+    item2: 5,
+    item3: 8,
+    item4: 7,
+  };
+
+  const [stock, setStock] = useState(initialStocks);
+
+  const handleAdd = (e, quantity) => {
+    const item = e.target.parentNode.getAttribute("name");
+    const previousStock = stock[item];
+    if (previousStock >= quantity) {
+      setStock({...stock, [item]: previousStock - quantity});
+      console.log("Sumar al carrito " + quantity + " unidades del " + item);
+    } else {
+      console.log("No hay stock suficiente");
+    }
+  };
+
   return (
     <div>
       <h1 style={{textAlign: "center"}}>
         {greeting}, desde aquí podrás ver un listado{" "}
       </h1>
       <ListGroup>
-        <ListGroup.Item variant="info">Item 1</ListGroup.Item>
-        <ListGroup.Item variant="info">Item 2</ListGroup.Item>
-        <ListGroup.Item variant="info">Item 3</ListGroup.Item>
-        <ListGroup.Item variant="info">Item 4</ListGroup.Item>
+        <ListGroup.Item variant="info" name="item1">
+          {" Item 1 "}
+          <ItemCount
+            stock={stock.item1}
+            initialValue={1}
+            handleAdd={handleAdd}
+          />
+        </ListGroup.Item>
+        <ListGroup.Item variant="info" name="item2">
+          {" Item 2 "}
+          <ItemCount
+            stock={stock.item2}
+            initialValue={1}
+            handleAdd={handleAdd}
+          />
+        </ListGroup.Item>
+        <ListGroup.Item variant="info" name="item3">
+          {" Item 3 "}
+          <ItemCount
+            stock={stock.item3}
+            initialValue={1}
+            handleAdd={handleAdd}
+          />
+        </ListGroup.Item>
+        <ListGroup.Item variant="info" name="item4">
+          {" Item 4 "}
+          <ItemCount
+            stock={stock.item4}
+            initialValue={2}
+            handleAdd={handleAdd}
+          />
+        </ListGroup.Item>
       </ListGroup>
     </div>
   );
