1cc1614f28f36811f148d75b817617baaf094281
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-05-18 22:11:10 -0300

    add item & item list components

---
+++ src/components/Item.js
@@ -0,0 +1,15 @@
+import ListGroup from "react-bootstrap/ListGroup";
+import ItemCount from "./ItemCount";
+
+export default function ItemList({handleAdd, name, description, stock, initialValue}) {
+
+  return (
+    <ListGroup.Item variant="info"> {name} - {description}
+        <ItemCount
+          stock={stock}
+          initialValue={initialValue}
+          handleAdd={handleAdd}
+        />
+      </ListGroup.Item>
+  );
+}
---
+++ src/components/ItemList.js
@@ -0,0 +1,14 @@
+import ListGroup from "react-bootstrap/ListGroup";
+import Item from "./Item";
+
+export default function ItemList({handleAdd, products}) {
+
+    const product = products.map((item)=> <Item name={item.name} description={item.description} stock={item.stock} initialValue={item.initialValue} handleAdd={handleAdd}/>)
+
+
+  return (
+      <ListGroup>
+        {product}
+      </ListGroup>
+  );
+}
