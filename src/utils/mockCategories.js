adf01b9f5a0fcb3a8936c64e4f34515b6a9d4098
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-05-24 11:50:42 -0300

    expand mock products & include mock categories

---
+++ src/utils/mockCategories.js
@@ -0,0 +1,6 @@
+export const MOCKCATEGORIES = [
+    {id: 1, name: "Vehículos"},
+    {id: 2, name: "Electrónica"},
+    {id: 3, name: "Libros"}
+  ];
+  
--- src/utils/mockProducts.js
+++ src/utils/mockProducts.js
@@ -1,7 +1,14 @@
 export const MOCKPRODUCTS = [
-  {id: 1, name: "Producto 1", price:1000, image:"https://placekitten.com/500/500", description: "Descripción 1", stock: 4, initialValue: 1},
-  {id: 2, name: "Producto 2", price:1000, image:"https://placekitten.com/400/400", description: "Descripción 2", stock: 3, initialValue: 1},
-  {id: 3, name: "Producto 3", price:1000, image:"https://placekitten.com/510/510", description: "Descripción 3", stock: 8, initialValue: 1},
-  {id: 4, name: "Producto 4", price:1000, image:"https://placekitten.com/600/400", description: "Descripción 4", stock: 5, initialValue: 1},
-  {id: 5, name: "Producto 5", price:1000, image:"https://placekitten.com/400/600", description: "Descripción 5", stock: 10, initialValue: 2},
+  {id: 1, name: "Producto 1", price:1000, image:"https://placekitten.com/500/500", description: "Descripción 1", category: 3, stock: 4, initialValue: 1},
+  {id: 2, name: "Producto 2", price:1000, image:"https://placekitten.com/400/400", description: "Descripción 2", category: 1, stock: 3, initialValue: 1},
+  {id: 3, name: "Producto 3", price:1000, image:"https://placekitten.com/510/510", description: "Descripción 3", category: 1, stock: 8, initialValue: 1},
+  {id: 4, name: "Producto 4", price:1000, image:"https://placekitten.com/600/400", description: "Descripción 4", category: 2, stock: 5, initialValue: 1},
+  {id: 5, name: "Producto 5", price:1000, image:"https://placekitten.com/400/600", description: "Descripción 5", category: 2, stock: 10, initialValue: 2},
+  {id: 6, name: "Producto 6", price:1000, image:"https://placekitten.com/510/510", description: "Descripción 6", category: 1, stock: 8, initialValue: 1},
+  {id: 7, name: "Producto 7", price:1000, image:"https://placekitten.com/600/400", description: "Descripción 7", category: 3, stock: 6, initialValue: 1},
+  {id: 8, name: "Producto 8", price:1000, image:"https://placekitten.com/400/600", description: "Descripción 8", category: 3, stock: 12, initialValue: 2},
+  {id: 9, name: "Producto 9", price:1000, image:"https://placekitten.com/510/510", description: "Descripción 9", category: 3, stock: 6, initialValue: 1},
+  {id: 10, name: "Producto 10", price:1000, image:"https://placekitten.com/600/400", description: "Descripción 10", category: 1, stock: 5, initialValue: 1},
+  {id: 11, name: "Producto 11", price:1000, image:"https://placekitten.com/400/600", description: "Descripción 11", category: 1, stock: 7, initialValue: 1},
+
 ];
