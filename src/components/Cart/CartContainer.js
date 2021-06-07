7b18860ab2e6b5eaf68ef99dc16189890d911b22
Mariano Fernandez <mariano.b.fernandez@gmail.com>
2021-06-07 13:55:16 -0300

    fix isInCart

--- src/components/Cart/CartContainer.js
+++ src/components/Cart/CartContainer.js
@@ -5,17 +5,15 @@ export default function CartContainer() {
   const cart = useCart();
 
   const productsInCart = cart.cartProducts.map(item => {
-    return (<div style={{display:"flex"}}>
-      <p>
-      {`ID PRODUCTO: ${item.id}, CANTIDAD: ${item.quantity} - `}
-      </p>
-      <Button onClick={()=>cart.removeItem(item.id)}>
+    return (
+      <div style={{display: "flex"}}>
+        <p>{`ID PRODUCTO: ${item.id}, CANTIDAD: ${item.quantity} - `}</p>
+        <Button onClick={() => cart.removeItem(item.id)}>
           Borrar Producto
-      </Button>
+        </Button>
       </div>
     );
   });
-
   return (
     <div>
       <h2>Productos en el carrito</h2>
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -8,30 +8,32 @@ export const CartProvider = ({children}) => {
   const [cartProducts, setCartProducts] = useState([]);
 
   const addItem = (quantity, product_id) => {
-      //TODO: Should only reject? Warn? Add to cartProducts duplicate product?
+    //TODO: Should only reject? Warn? Add to cartProducts duplicate product?
     if (cartProducts.filter(item => item.id === product_id).length === 0) {
-      console.log("added to", cartProducts)
       setCartProducts([...cartProducts, {id: product_id, quantity}]);
     }
     //placeholder LOG
     console.log(cartProducts, product_id, quantity);
   };
 
-  const removeItem = (product_id) => {
+  const removeItem = product_id => {
     const filtered = cartProducts.filter(item => item.id !== product_id);
     setCartProducts(filtered);
-  }
+  };
 
   const clear = () => {
-      setCartProducts([]);
-  }
+    setCartProducts([]);
+  };
 
-  const isInCart = (product_id) => {
-      return cartProducts.filter(item => item.id === product_id) === []
-  }
+  const isInCart = product_id => {
+    return !(
+      cartProducts.filter(item => +item.id === +product_id).length === 0
+    );
+  };
 
   return (
-    <CartContext.Provider value={{cartProducts, addItem, removeItem, clear, isInCart}}>
+    <CartContext.Provider
+      value={{cartProducts, addItem, removeItem, clear, isInCart}}>
       {children}
     </CartContext.Provider>
   );
